0ff6a2fae5fd638884878d981bd8d3fb3b218e85
Michele Dolfi <michele.dolfi@gmail.com>
2015-02-22 23:09:50 +0100

    improve student admin

--- ipho_core/admin.py
+++ ipho_core/admin.py
@@ -9,7 +9,11 @@ class DelegationAdmin(admin.ModelAdmin):
 
 class StudentAdmin(admin.ModelAdmin):
     fields = ('code', ('first_name', 'last_name'), 'delegation',)
-
+    list_display = ('code', 'last_name', 'first_name', 'delegation',)
+    search_fields = ('last_name', 'first_name')
+    list_editable = ('first_name', 'last_name', 'delegation')
+    list_filter = ('delegation',)
+    
 
 admin.site.register(Delegation, DelegationAdmin)
 admin.site.register(Student, StudentAdmin)
