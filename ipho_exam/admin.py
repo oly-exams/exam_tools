69c17e2e3845a227c5716e82c91b97973da4d50b
Michele Dolfi <michele.dolfi@gmail.com>
2015-02-22 19:15:33 +0100

    improve admin settings

--- ipho_exam/admin.py
+++ ipho_exam/admin.py
@@ -3,8 +3,16 @@ from ipho_exam.models import Language, Exam, Question, VersionNode, TranslationN
 
 # Register your models here.
 
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class ExamAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
 admin.site.register(Language)
-admin.site.register(Exam)
+admin.site.register(Exam, ExamAdmin)
 admin.site.register(Question)
 admin.site.register(VersionNode)
 admin.site.register(TranslationNode)
