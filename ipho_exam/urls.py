821161ea4adf1a82def4d40a879c5d5e89d48be0
Michele Dolfi <michele.dolfi@gmail.com>
2015-03-26 23:38:15 +0100

    populate editor template with links to exam / questions

--- ipho_exam/urls.py
+++ ipho_exam/urls.py
@@ -5,7 +5,14 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('ipho_exam.views',
     
     url(r'^$', 'index', name='index'),
-    url(r'^editor/?$', 'editor', name='editor'),
+    
+    url(r'^editor/?$', 'editor'),
+    url(r'^editor/(?P<exam_id>\d+)$', 'editor', name='editor-exam'),
+    url(r'^editor/(?P<exam_id>\d+)/question/(?P<question_id>\d+)$', 'editor', name='editor-question'),
+    url(r'^editor/(?P<exam_id>\d+)/question/(?P<question_id>\d+)/lang/(?P<lang_id>\d+)?$', 'editor', name='editor-lang'),
+    url(r'^editor/(?P<exam_id>\d+)/question/(?P<question_id>\d+)/lang/(?P<lang_id>\d+)/(?P<orig_id>\d+)?$', 'editor', name='editor-orig'),
+    url(r'^editor/(?P<exam_id>\d+)/question/(?P<question_id>\d+)/lang/(?P<lang_id>\d+)/(?P<orig_id>\d+)v(?P<orig_v>\d+)?$', 'editor', name='editor-orig-version'),
+    # url(r'^editor/(?P<exam_id>\d+)/question/(?P<question_id>\d+)/lang/(?P<lang_id>\d+)/(?P<orig_id>\d+)v(?P<orig_v1>\d+)v(?P<orig_v2>\d+)?$', 'editor', name='editor-orig-version'),
     
     
     url(r'^test/', include('ipho_exam.urls_test', namespace='test')),
--- ipho_exam/views.py
+++ ipho_exam/views.py
@@ -7,11 +7,33 @@ from django.core.urlresolvers import reverse
 from django.templatetags.static import static
 
 
+from ipho_exam.models import Exam, Question
+
+
 def index(request):
     return render_to_response('ipho_exam/index.html',
                               context_instance=RequestContext(request))
 
-def editor(request):
-    return render_to_response('ipho_exam/editor.html',
-                              context_instance=RequestContext(request))
+def editor(request, exam_id=None, question_id=None, lang_id=None, orig_id=0, orig_v=None):
+    context = {'exam_id'     : exam_id,
+               'question_id' : question_id,
+               'lang_id'     : question_id,
+               'orig_id'     : orig_id,
+               'orig_v'      : orig_v,
+               }
+    exam_list = Exam.objects.filter(hidden=False) # TODO: allow admin to see all exams
+    context['exam_list'] = exam_list
+    
+    if exam_id is not None:
+        exam = Exam.objects.get(id=exam_id)
+        context['exam'] = exam
+    if question_id is not None:
+        question = Question.objects.get(id=question_id)
+        context['question'] = question
+    elif exam is not None and exam.question_set.count() > 0:
+        question = exam.question_set.all()[0]
+        context['question'] = question
+    
+    
+    return render(request, 'ipho_exam/editor.html', context)
 
