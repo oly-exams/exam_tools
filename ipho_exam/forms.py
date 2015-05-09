e47f035599135b2064f17e2c09cb7cdd0c9bfb58
Michele Dolfi <michele.dolfi@gmail.com>
2015-05-09 21:16:42 +0200

    add Polyglossia fields

--- ipho_exam/forms.py
+++ ipho_exam/forms.py
@@ -9,13 +9,13 @@ class LanguageForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(LanguageForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.layout = Layout(Field('name', placeholder='Name'),)
+        self.helper.layout = Layout(Field('name', placeholder='Name'),Field('polyglossia'))
         self.helper.html5_required = True
-        self.helper.form_show_labels = False
+        # self.helper.form_show_labels = False
         self.helper.form_method = 'post'
         self.helper.form_action = 'exam:index'
         self.helper.add_input(Submit('submit', 'Create'))
         
     class Meta:
         model = Language
-        fields = ['name']
+        fields = ['name','polyglossia']
