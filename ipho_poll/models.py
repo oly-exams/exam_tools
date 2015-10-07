390809cc760a9538104d57be9396eb29603e5d56
SebastianJL <johannes.lade@yahoo.com>
2015-10-07 20:21:30 +0200

    Redesigned models for ipho2016
    I cleaned the models of uneccessary functionality and added a new one. This includes:
    
     - Removed the pub_date and stripped the was_published_recently() method from Question.
    
     - Added a vote model which connects the Delegations to the choices they made.

--- ipho_poll/models.py
+++ ipho_poll/models.py
@@ -1,21 +1,18 @@
 import datetime
 
 from django.db import models
-from django.utils import timezone
-
-
+from ipho_core.models import Delegation
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('Date published')
+    STATUS_CHOICES = (
+        (0, "draft"),
+        (1, "live"),
+        (2, "closed"),
+    )
+    status = models.IntegerField(choice=STATUS_CHOICES, default=0) 
     def __str__(self):
         return self.question_text
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
@@ -23,3 +20,7 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+class vote(models.Model):
+    choice = models.ForeignKey(Choice)
+    delegation = models.ForeignKey(Delegation)    
