e428986a57da66f844a6f8e260cc60a85f0693eb
SebastianJL <johannes.lade@yahoo.com>
2015-10-07 20:41:25 +0200

    Removed uneccessary urls.
    Now there are only two remaining urls.
    
     - One is for the AdminOverview for managing the polls.
    
     - One is for the DelegationOverview for voting.

--- ipho_poll/urls.py
+++ ipho_poll/urls.py
@@ -1,16 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import patterns, url
 
 from . import views
 
-urlpatterns = [
+urlpatterns = patterns('',
     #admin urls
-    url(r'^admin/$', views.AdminOverview.as_view(), name='adminOverview'),
-    #url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^admin/$', views.AdminOverview, name='adminOverview'),
 
     #user urls
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^$', views.DelegationOverview, name='DelegationOverview'),
 
-]
+)
