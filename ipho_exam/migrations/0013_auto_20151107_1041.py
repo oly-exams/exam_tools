31a6814c7989b5b33bdb251ea5e6cda43bbb2093
Michele Dolfi <michele.dolfi@gmail.com>
2015-11-07 10:41:50 +0100

    minor old migration

---
+++ ipho_exam/migrations/0013_auto_20151107_1041.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ipho_exam', '0012_exam_feedback_active'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='question',
+            name='position',
+            field=models.PositiveSmallIntegerField(help_text=b'Sorting index inside one exam'),
+            preserve_default=True,
+        ),
+    ]
