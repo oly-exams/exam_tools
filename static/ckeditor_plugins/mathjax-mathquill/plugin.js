3903d8471cf248a5547376308b7ee73a9ff118fb
Michele Dolfi <michele.dolfi@gmail.com>
2016-01-09 17:55:44 +0100

    close #3. Working toolbar

--- static/ckeditor_plugins/mathjax-mathquill/plugin.js
+++ static/ckeditor_plugins/mathjax-mathquill/plugin.js
@@ -1,6 +1,5 @@
 ﻿/**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * Copyright (c) 2015-2016, IPhO 2016 - Michele Dolfi.
  */
 
 /**
