62de0c240dfad438dc514b837478cc47ad7bb272
Johannes Lade <johannes.lade@yahoo.com>
2016-01-15 08:27:06 +0100

    new migrations for ipho_poll app

--- ipho_poll/migrations/0001_initial.py
+++ ipho_poll/migrations/0001_initial.py
@@ -7,6 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('ipho_core', '0005_iphoperm'),
     ]
 
     operations = [
@@ -17,18 +18,37 @@ class Migration(migrations.Migration):
                 ('choice_text', models.CharField(max_length=200)),
                 ('votes', models.IntegerField(default=0)),
             ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='Question',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('question_txt', models.CharField(max_length=200)),
-                ('pub_date', models.DateTimeField(verbose_name='date_published')),
+                ('question_text', models.CharField(max_length=200)),
+                ('pub_date', models.DateTimeField(verbose_name=b'date published')),
+                ('status', models.IntegerField(default=0, choices=[(0, b'draft'), (1, b'live'), (2, b'closed')])),
             ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Vote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('choice', models.ForeignKey(to='ipho_poll.Choice')),
+                ('delegation', models.ForeignKey(to='ipho_core.Delegation')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.AddField(
             model_name='choice',
             name='question',
-            field=models.ForeignKey(to='polls.Question'),
+            field=models.ForeignKey(to='ipho_poll.Question'),
+            preserve_default=True,
         ),
     ]
