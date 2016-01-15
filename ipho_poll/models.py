abbfdba1337cf8df4f8e8efae228d758d4031932
Johannes Lade <johannes.lade@yahoo.com>
2016-01-15 08:29:55 +0100

    Corrected spelling error. Added pub_date field for Question Class

--- ipho_poll/models.py
+++ ipho_poll/models.py
@@ -5,12 +5,13 @@ from ipho_core.models import Delegation
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
     STATUS_CHOICES = (
         (0, "draft"),
         (1, "live"),
         (2, "closed"),
     )
-    status = models.IntegerField(choice=STATUS_CHOICES, default=0) 
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0) 
     def __str__(self):
         return "Question Text: " + self.question_text + " / Status: " + self.status 
 
