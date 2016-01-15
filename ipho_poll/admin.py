5ccf9eef930f358e7603f33f306943b7776eb43c
Johannes Lade <johannes.lade@yahoo.com>
2016-01-15 08:24:54 +0100

    was published recently field deletet

--- ipho_poll/admin.py
+++ ipho_poll/admin.py
@@ -13,7 +13,7 @@ class QuestionAdmin(admin.ModelAdmin):
 					 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_display = ('question_text', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
