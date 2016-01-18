2b0291980466cd38a3a6378ab96a92c467bc39f4
Johannes Lade <johannes.lade@yahoo.com>
2016-01-18 15:41:03 +0100

    added url for adding questions

--- ipho_poll/urls.py
+++ ipho_poll/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('ipho_poll.views',
     #admin urls
     url(r'^admin/$', 'adminIndex', name='adminIndex'),
+    url(r'^admin/question/add$', 'addQuestion', name='addQuestion'),
+
 
     #user urls
     url(r'^$', 'delegationIndex', name='delegationIndex'),
