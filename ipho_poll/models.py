71378eb44f2951566b5496292fb5f097a6d24700
Johannes Lade <johannes.lade@yahoo.com>
2016-01-21 11:17:16 +0100

    Set default value for pub_date field of Question model to django.utils.timezone.now

--- ipho_poll/models.py
+++ ipho_poll/models.py
@@ -1,19 +1,18 @@
-import datetime
-
+from django.utils import timezone
 from django.db import models
 from ipho_core.models import Delegation
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', default=timezone.now)
     STATUS_CHOICES = (
         (0, "draft"),
         (1, "live"),
         (2, "closed"),
     )
-    status = models.IntegerField(choices=STATUS_CHOICES, default=0) 
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
     def __str__(self):
-        return self.question_text 
+        return self.question_text
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
@@ -24,4 +23,4 @@ class Choice(models.Model):
 
 class Vote(models.Model):
     choice = models.ForeignKey(Choice)
-    delegation = models.ForeignKey(Delegation)    
+    delegation = models.ForeignKey(Delegation)
