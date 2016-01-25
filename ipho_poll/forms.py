fa49a27639daccdbfbf583efbea350edabfc5ecc
Johannes Lade <johannes.lade@yahoo.com>
2016-01-25 10:15:23 +0100

    added Form for 'choice' model.

--- ipho_poll/forms.py
+++ ipho_poll/forms.py
@@ -8,7 +8,7 @@ from django.utils.safestring import mark_safe
 from django.core.exceptions import ValidationError
 
 
-from ipho_poll.models import Question
+from ipho_poll.models import Question, Choice
 
 
 
@@ -20,7 +20,19 @@ class QuestionForm(ModelForm):
             self.helper.html5_required = True
             self.helper.form_show_labels = True
 
-
         class Meta:
             model = Question
             fields = ['question_text']
+
+
+class ChoiceForm(ModelForm):
+        def __init__(self, *args, **kwargs):
+            super(ChoiceForm, self).__init__(*args, **kwargs)
+            self.helper = FormHelper()
+            self.helper.layout = Layout(Field('choice_text', placeholder='Enter choice text'))
+            self.helper.html5_required = True
+            self.helper.form_show_labels = True
+
+        class Meta:
+            model = Choice
+            fields = ['choice_text']
