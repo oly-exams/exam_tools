705b9368ed9f6a40c165aec6fd91cf82b6cca1d7
Johannes Lade <johannes.lade@yahoo.com>
2016-01-25 10:06:43 +0100

    removed 'votes' integerField() from 'Choice' model as votes will be handled with a seperate 'vote' model.

--- ipho_poll/models.py
+++ ipho_poll/models.py
@@ -17,7 +17,6 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
 
