e8770eba0b844ccce1a0c5829f46190f7aec0aba
Johannes Lade <johannes.lade@yahoo.com>
2016-01-26 10:56:10 +0100

    updated admin page for question model. It now shows the status of the question (draft, live, closed) and lets you change the status.

--- ipho_poll/admin.py
+++ ipho_poll/admin.py
@@ -4,17 +4,17 @@ from .models import Question, Choice
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
+    extra = 2
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('Question information',	{'fields': ['question_text']}),
+        ('Question information',	{'fields': ['question_text', 'status']}),
         ('Date information',		{'fields': ['pub_date'],
 					 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date')
-    list_filter = ['pub_date']
+    list_display = ('question_text', 'status', 'pub_date')
+    list_filter = ['pub_date', 'status']
     search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
