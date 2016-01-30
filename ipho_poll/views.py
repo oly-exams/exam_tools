fd83c66b683b7e78dfa52261430429e3f6a94906
Johannes Lade <johannes.lade@yahoo.com>
2016-01-30 01:22:24 +0100

    implemented deletion button for questions
    Each row of the question table now contains a delete button which
    toggles a modal which asks to confirm deletion.
    
    Changes in different files:
      - ipho_poll/views.py. Added deleteQuestion() view which deletes the
        question passed in the get request along with the associated
        choices.
      - templates/ipho_poll/staffIndex.html. Added Jquery which handels the
        button click for the delete button and the ajax protocoll to the
        deleteQuestion view.
      - templates/ipho_poll/tables/drafted_question.html. Added a delete
        modal.

--- ipho_poll/views.py
+++ ipho_poll/views.py
@@ -15,7 +15,9 @@ from .models import Question, Choice, Vote
 
 from .forms import QuestionForm, ChoiceForm, VoteForm
 from .forms import ChoiceFormHelper
-#admin views
+
+
+#staff views
 
 @login_required
 @permission_required('iphoperm.is_staff')
@@ -60,7 +62,7 @@ def addQuestion(request):
             choice_text_list.append(new_choice.choice_text)
         return JsonResponse({
                     'success'           : True,
-                    'message'           : '<strong> The question has successfully been added!</strong>',
+                    'message'           : '<strong> The voting has successfully been added!</strong>',
                     'new_question_text' : new_question.question_text,
                     'new_question_pk'   : new_question.pk,
                     'choice_text_list'  : choice_text_list,
@@ -78,12 +80,32 @@ def addQuestion(request):
                                         )
                     )
         return JsonResponse({
-                    'title'         : 'Create New Voting',
+                    'title'         : 'Create new voting',
                     'form'          : form_html,
                     'success'       : False,
-                    'message'       : 'The question could not be added.',
                 })
 
+@login_required
+@permission_required('iphoperm.is_staff')
+@ensure_csrf_cookie
+def deleteQuestion(request, question_pk):
+    question = get_object_or_404(Question, pk=question_pk)
+    choice_list = Choice.objects.filter(question=question)
+    # try:
+    question.delete()
+    for choice in choice_list:
+        choice.delete()
+    # except Exception:
+    #     return JsonResponse({
+    #                 'success'   : False,
+    #                 'message'   : Exception.message,
+    #     })
+    #
+
+    return JsonResponse({
+                'success'   : True,
+                'message'   : "<strong>The voting #" + question_pk + " has been deleted successfully.</strong>"
+    })
 
 
 
@@ -122,70 +144,3 @@ def addVote(request):
             return JsonResponse({
                         'message'   : 'Thanks, your vote has been saved.'
             })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#class IndexView(generic.ListView):
-#    template_name = 'polls/index.html'
-#    context_object_name = 'latest_question_list'
-#
-#    def get_queryset(self):
-#        """
-#        Return the last five published questions (not including those set to be
-#        published in the future and those with no choices).
-#        """
-#        return Question.objects.filter(
-#               choice__gte=1, pub_date__lte=timezone.now()
-#        ).distinct().order_by('-pub_date')[0:5]
-#
-#
-#class DetailView(generic.DetailView):
-#    model = Question
-#    template_name = 'polls/detail.html'
-#    def get_queryset(self):
-#        """
-#        Excludes any questions that aren't published yet.
-#        Excludes any questions with no choices.
-#        """
-#        return Question.objects.filter(
-#            choice__gte=1, pub_date__lte=timezone.now()
-#        ).distinct()
-#
-#
-#class ResultsView(generic.DetailView):
-#    model = Question
-#    template_name = 'polls/results.html'
-#    def get_queryset(self):
-#        """
-#        Excludes any questions that aren't published yet.
-#        """
-#        return Question.objects.filter(pub_date__lte=timezone.now())
-#
-#
-#def vote(request, question_id):
-#    p = get_object_or_404(Question, pk=question_id)
-#    try:
-#        selected_choice = p.choice_set.get(pk=request.POST['choice'])
-#    except (KeyError, Choice.DoesNotExist):
-#        #Redisplay the question voting form
-#        return render(request, 'polls/detail.html', {'question': p, 'error_message': "You didn't select a choice.",
-#        })
-#    else:
-#        selected_choice.votes += 1
-#        selected_choice.save()
-#        #Always return an HttpResponse after succesfully dealing with POST data. This prevents data from being posted twice if a user hits the Back button.
-#        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
