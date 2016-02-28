4252f617cb0ad92e87dc9a78df91dc18a5842925
Michele Dolfi <michele.dolfi@gmail.com>
2016-02-28 17:58:38 +0100

    improve text and mention Noto fonts

--- ipho_exam/forms.py
+++ ipho_exam/forms.py
@@ -44,8 +44,8 @@ class LanguageForm(ModelForm):
         model = Language
         fields = ['name','direction','polyglossia','font']
         labels = {
-                   'polyglossia': 'Language style <a href="#" data-toggle="popover" data-trigger="hover" data-container="body" data-content="Select a lanugage similar to yours. This will improve the final typesetting, e.g. allowing correct hyphenation."><span class="glyphicon glyphicon-info-sign"></span></a>',
-                   'font': 'Font in PDF <a href="#" data-toggle="popover" data-trigger="hover" data-html="true" data-container="body" data-content="For most languages choose <emph>Arial</emph>. More fonts can be added on request."><span class="glyphicon glyphicon-info-sign"></span></a>',
+                   'polyglossia': 'Language style <a href="#" data-toggle="popover" data-trigger="hover" data-html="true" data-container="body" data-content="Select a lanugage similar to yours or leave <emph>english</emph>. This will improve the final typesetting, e.g. allowing correct hyphenation."><span class="glyphicon glyphicon-info-sign"></span></a>',
+                   'font': 'Font in PDF <a href="#" data-toggle="popover" data-trigger="hover" data-html="true" data-container="body" data-content="For most languages choose <emph>Noto Sans</emph>. Preview of Noto fonts is available <a href=\'https://www.google.com/get/noto\' target=\'_blank\'>here</a>. More fonts can be added on request."><span class="glyphicon glyphicon-info-sign"></span></a>',
                }
 
 class FigureForm(ModelForm):
