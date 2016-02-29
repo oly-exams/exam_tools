8ab615110fcc65b51632dbf6b53753e478ab005f
Michele Dolfi <michele.dolfi@gmail.com>
2016-02-29 21:50:37 +0100

    make language name not unique

---
+++ ipho_exam/migrations/0026_auto_20160229_2149.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ipho_exam', '0025_auto_20160228_1738'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='language',
+            name='name',
+            field=models.CharField(max_length=100),
+        ),
+    ]
