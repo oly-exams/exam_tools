9d781470537240bc9d28f21e894220b1b914a7e9
Michele Dolfi <michele.dolfi@gmail.com>
2016-03-07 01:15:31 +0100

    redirect to wizard after login

--- ipho_core/views.py
+++ ipho_core/views.py
@@ -1,13 +1,15 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
 
 from ipho_core.models import AutoLogin, User
 
 def autologin(request, token):
     user = authenticate(token=token)
+    redirect_to = reverse('exam:wizard')
     if user:
         login(request, user)
-        return redirect('/')
+        return redirect(redirect_to)
     else:
-        return redirect(settings.LOGIN_URL+'?next=/')
+        return redirect(settings.LOGIN_URL+'?next={}'.format(redirect_to))
