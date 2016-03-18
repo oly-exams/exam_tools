e24feeb896cf0ed03e17e34fd1ed0ba05a19fb6f
Johannes Lade <johannes.lade@yahoo.com>
2016-03-18 16:37:39 +0100

    Added StatusForm to change status of question.

--- ipho_poll/forms.py
+++ ipho_poll/forms.py
@@ -1,11 +1,10 @@
 from django import forms
-from django.forms import ModelForm, Form
+from django.forms import ModelForm, Form, HiddenInput, DateInput
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Field, MultiField, Div
+from crispy_forms.layout import Submit, Layout, Field, MultiField, Div, HTML
 from django.utils.safestring import mark_safe
 
 from django.core.exceptions import ValidationError
-
 
 from ipho_poll.models import Question, Choice, Vote
 
@@ -26,6 +25,26 @@ class QuestionForm(ModelForm):
         fields = ['question_text']
 
 
+class StatusForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(StatusForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+                                    Field('end_date', id="modal-datetimepicker"),
+                                    Field('status')
+                                    )
+        self.helper.html5_required = True
+        self.helper.form_show_labels = True
+        self.helper.form_tag = False
+        self.helper.disable_csrf = True
+        self.helper.include_media = True
+    class Meta:
+        model = Question
+        fields = ['end_date', 'status']
+        localized_fields = ['end_date',]
+        widgets={'status': HiddenInput(), }
+
+
 class ChoiceForm(ModelForm):
     class Meta:
         model = Choice
@@ -43,6 +62,7 @@ class ChoiceFormHelper(FormHelper):
         self.html5_required = True
         self.form_tag = False
         self.disable_csrf = True
+
 
 class VoteForm(ModelForm):
     class Meta:
