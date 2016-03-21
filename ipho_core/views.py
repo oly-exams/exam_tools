03294da46efd804ae17a3672cecca04298343aa7
Michele Dolfi <michele.dolfi@gmail.com>
2016-03-21 16:04:50 +0100

    redirect to the home after autologin

--- ipho_core/views.py
+++ ipho_core/views.py
@@ -7,7 +7,7 @@ from ipho_core.models import AutoLogin, User
 
 def autologin(request, token):
     user = authenticate(token=token)
-    redirect_to = reverse('exam:wizard')
+    redirect_to = reverse('home')
     if user:
         login(request, user)
         return redirect(redirect_to)
