86448c95b88882f1ddfac9f359b5e15c68b4b614
Johannes Lade <johannes.lade@yahoo.com>
2016-04-23 00:51:07 +0200

    Initial migrations

--- ipho_marking/migrations/0001_initial.py
+++ ipho_marking/migrations/0001_initial.py
@@ -7,6 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('ipho_core', '0007_autologin'),
         ('ipho_exam', '0030_language_style'),
     ]
 
@@ -15,13 +16,18 @@ class Migration(migrations.Migration):
             name='Marking',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('points', models.IntegerField()),
+                ('points', models.FloatField()),
+                ('comment', models.TextField()),
+                ('version', models.CharField(max_length=1, choices=[(b'O', b'Organizers'), (b'D', b'Delegation'), (b'F', b'Final')])),
             ],
         ),
         migrations.CreateModel(
             name='QuestionPoints',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=2)),
+                ('max_points', models.FloatField()),
+                ('position', models.PositiveSmallIntegerField(help_text=b'Sorting index inside one question')),
                 ('question', models.ForeignKey(to='ipho_exam.Question')),
             ],
         ),
@@ -29,5 +35,10 @@ class Migration(migrations.Migration):
             model_name='marking',
             name='question_points',
             field=models.ForeignKey(to='ipho_marking.QuestionPoints'),
+        ),
+        migrations.AddField(
+            model_name='marking',
+            name='student',
+            field=models.ForeignKey(to='ipho_core.Student'),
         ),
     ]
