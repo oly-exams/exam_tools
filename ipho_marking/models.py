213a9c57dafbde9d80ed0fe84baffb5144e3fd8e
Johannes Lade <johannes.lade@yahoo.com>
2016-04-23 00:50:21 +0200

    Create models for marking app

--- ipho_marking/models.py
+++ ipho_marking/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+from ipho_core.models import Student
+
+from ipho_exam.models import Exam, Question
+
+class QuestionPoints(models.Model):
+    question = models.ForeignKey(Question)
+    name = models.CharField(max_length=2)
+    max_points = models.FloatField()
+    position = models.PositiveSmallIntegerField(help_text='Sorting index inside one question')
+
+
+
+class Marking(models.Model):
+    question_points = models.ForeignKey(QuestionPoints)
+    student = models.ForeignKey(Student)
+    points = models.FloatField()
+    comment = models.TextField()
+    MARKING_VERSIONS = (
+        ('O', 'Organizers'),
+        ('D', 'Delegation'),
+        ('F', 'Final'),
+    )
+    version = models.CharField(max_length=1, choices=MARKING_VERSIONS)
+
+    def __unicode__(self):
+        return u'{} [{} / {}]'.format(self.question_points.name, self.points, self.question_points.max_points)
