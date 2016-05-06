ff3d4728a8618ccec2cea1d018785e5791161a5e
Michele Dolfi <michele.dolfi@gmail.com>
2016-05-06 22:28:37 +0200

    small fix in urls

--- ipho_marking/urls.py
+++ ipho_marking/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
 ## Delegation views
     url(r'^/?$', views.delegation_summary, name='delegation-summary'),
-    url(r'^/detail/(?P<stud_id>\d+)/exam/(?P<exam_id>\d+)/question/(?P<question_id>\d+)$', views.delegation_stud_detail, name='delegation-stud-detail'),
-    url(r'^/confirm/(?P<exam_id>\d+)$', views.delegation_confirm, name='delegation-confirm'),
+    url(r'^detail/(?P<stud_id>\d+)/exam/(?P<exam_id>\d+)/question/(?P<question_id>\d+)$', views.delegation_stud_detail, name='delegation-stud-detail'),
+    url(r'^confirm/(?P<exam_id>\d+)$', views.delegation_confirm, name='delegation-confirm'),
 
 ## Markers / staff views
     url(r'^import/?$', views.import_exam, name='import-exam'),
