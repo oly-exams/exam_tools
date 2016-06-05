194d9b06c717dc723d4116cd2f76d92996a345bf
Johannes Lade <johannes.lade@yahoo.com>
2016-06-05 11:43:29 +0200

    Migrated new model "Like"

---
+++ ipho_exam/migrations/0050_like.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ipho_core', '0008_auto_20160528_1544'),
+        ('ipho_exam', '0049_auto_20160531_1923'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Like',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('status', models.CharField(default=b'N', max_length=1, choices=[(b'N', b'None'), (b'L', b'Liked'), (b'U', b'Unliked')])),
+                ('Feedback', models.ForeignKey(to='ipho_exam.Feedback')),
+                ('delegation', models.ForeignKey(to='ipho_core.Delegation')),
+            ],
+        ),
+    ]
---
+++ ipho_exam/migrations/0051_auto_20160605_1133.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ipho_exam', '0050_like'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='like',
+            old_name='Feedback',
+            new_name='feedback',
+        ),
+    ]
