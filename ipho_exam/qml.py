a962c6e6e8f6501575c18278699cf991dfc306bc
Dominik Gresch <greschd@gmx.ch>
2016-06-07 21:48:10 +0000

    changed ipho2016.cls to not contain counters, added the corresponding attributes to subquestion

--- ipho_exam/qml.py
+++ ipho_exam/qml.py
@@ -386,7 +386,11 @@ class QMLsubquestion(QMLobject):
         return 'Subquestion, %spt' % self.attributes['points']
 
     def tex_begin(self):
-        return u'\\begin{QTF}{%s}{%s}{%s}\n' % self.attributes['points'], self.attributes['part_nr'], self.attributes['question_nr']
+        return u'\\begin{QTF}{%s}{%s}{%s}\n' % (
+            self.attributes['points'], 
+            self.attributes['part_nr'], 
+            self.attributes['question_nr']
+        )
     def tex_end(self):
         return '\\end{QTF}\n\n'
     def xhtml_begin(self):
@@ -465,6 +469,8 @@ class QMLfigure(QMLobject):
     has_text = False
     has_children = True
     lang = None
+
+    default_attributes = {'figid': ''}
 
     def fig_query(self):
         query = {}
