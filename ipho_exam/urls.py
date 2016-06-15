3e66a9948e3d8658c046b070bf01c060011d27ce
Michele Dolfi <michele.dolfi@gmail.com>
2016-06-15 14:53:34 +0200

    remove UI for sorting

--- ipho_exam/urls.py
+++ ipho_exam/urls.py
@@ -62,7 +62,6 @@ urlpatterns = patterns('ipho_exam.views',
     url(r'^figure/(?P<fig_id>\d+)/(?P<lang_id>\d+)/export.pdf$', 'figure_export', {'output_format': 'pdf'}, name='figure-lang-export-pdf'),
 
     url(r'^admin/?$', 'admin_list', name='admin'),
-    url(r'^admin/(?P<exam_id>\d+)/sort$', 'admin_sort', name='admin-sort'),
     url(r'^admin/(?P<exam_id>\d+)/(?P<question_id>\d+)/newversion$', 'admin_new_version', name='admin-new-version'),
     url(r'^admin/(?P<question_id>\d+)/import$', 'admin_import_version', name='admin-import-version'),
     url(r'^admin/(?P<exam_id>\d+)/(?P<question_id>\d+)/v(?P<version_num>\d+)/accept$', 'admin_accept_version', name='admin-accept-version'),
