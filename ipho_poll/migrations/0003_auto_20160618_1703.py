56d62cb71800c7eaea6f6209d50e3ab1316fb5eb
Michele Dolfi <michele.dolfi@gmail.com>
2016-06-18 17:04:10 +0200

    add fields

---
+++ ipho_poll/migrations/0003_auto_20160618_1703.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ipho_poll', '0002_auto_20160613_1653'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='question',
+            name='implementation',
+            field=models.PositiveSmallIntegerField(default=0, choices=[(0, b'Not implemented'), (1, b'Implemented')]),
+        ),
+        migrations.AddField(
+            model_name='question',
+            name='vote_result',
+            field=models.PositiveSmallIntegerField(default=0, choices=[(0, b'In progress'), (1, b'Rejected'), (2, b'Accepted')]),
+        ),
+    ]
