18a49aaac3cc24f68d8460bc4b2b7ee2f6470cfb
Michele Dolfi <michele.dolfi@gmail.com>
2016-06-26 10:54:29 +0200

    more exceptions

--- ipho_exam/exceptions.py
+++ ipho_exam/exceptions.py
@@ -1,6 +1,12 @@
+from django.http import HttpResponseForbidden
+
 
 class IphoExamException(Exception):
     def __init__(self, response):
         self.response = response
     def __unicode__(self):
         return u'IPhO Exam Exception. Reponse: {}'.format(self.response)
+
+class IphoExamForbidden(Exception):
+    def __init__(self, msg):
+        super(IphoExamForbidden, self).__init__(HttpResponseForbidden(msg))
