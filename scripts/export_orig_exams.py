503dfc3fb9d855f72fb71c7cf0ce8efbc8b5f0a0
Michele Dolfi <michele.dolfi@gmail.com>
2016-07-06 13:22:23 +0200

    fix some script

--- scripts/export_orig_exams.py
+++ scripts/export_orig_exams.py
@@ -14,21 +14,27 @@ def save(objs, fname):
             stream=open(fname, 'w'))
 
 
+def save_with_pk(objs, fname):
+    serializers.serialize('json', objs, indent=2,
+            use_natural_foreign_keys=False,
+            use_natural_primary_keys=False,
+            stream=open(fname, 'w'))
+
 exams = Exam.objects.filter(name__in=['Theory', 'Experiment'])
 save(exams, '031_exams.json')
 
-quesions = Question.objects.filter(exam=exam)
-save(quesions, '032_questions.json')
+questions = Question.objects.filter(exam=exams)
+save(questions, '032_questions.json')
 
 figures = Figure.objects.all()
-save(figures, '033_figures.json')
+save_with_pk(figures, '033_figures.json')
 
-nodes = VersionNode.objects.filter(question=questions).oder_by('-version')
+nodes = VersionNode.objects.filter(question=questions).order_by('-version')
 last_nodes = []
-last_nodes_incl = set()
+last_nodes_incl = []
 for node in nodes:
-    if node.pk in last_nodes_incl:
+    if node.question.pk in last_nodes_incl:
         continue
     last_nodes.append(node)
-    last_nodes_incl.add(node.pk)
+    last_nodes_incl.append(node.question.pk)
 save(last_nodes, '034_content_nodes.json')
