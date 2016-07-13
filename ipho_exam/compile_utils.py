26dae63e34bab06d8b26e11cf5bdae0978d81432
Michele Dolfi <michele.dolfi@gmail.com>
2016-07-13 15:33:12 +0200

    move to new utils file

---
+++ ipho_exam/compile_utils.py
@@ -0,0 +1,140 @@
+# coding=utf-8
+from django.shortcuts import get_object_or_404
+from django.http import HttpRequest
+from django.template import RequestContext
+
+from django.core.urlresolvers import reverse
+from django.core.context_processors import csrf
+from crispy_forms.utils import render_crispy_form
+from django.template.loader import render_to_string
+
+from django.conf import settings
+from ipho_core.models import Delegation, Student
+from ipho_exam.models import Exam, Question, VersionNode, TranslationNode, PDFNode, Language, Figure, Feedback, StudentSubmission, ExamAction
+from ipho_exam import qml, tex, pdf, qquery, fonts, iphocode
+
+import ipho_exam
+from ipho_exam import tasks
+import celery
+from celery.result import AsyncResult
+
+OFFICIAL_LANGUAGE = 1
+OFFICIAL_DELEGATION = getattr(settings, 'OFFICIAL_DELEGATION')
+
+def student_exam_document(questions, student_languages, cover=None, commit=False):
+    meta = {}
+    meta['num_pages'] = 0
+    meta['barcode_num_pages'] = 0
+    meta['barcode_base'] = ''
+    meta['etag'] = ''
+    meta['filename'] = ''
+    all_barcodes = []
+    all_docs = []
+    if cover is not None:
+        body = render_to_string('ipho_exam/tex/exam_cover.tex', RequestContext(HttpRequest(), cover)).encode("utf-8")
+        question_pdf = pdf.compile_tex(body, [])
+        q = questions[0]
+        s = student_languages[0].student
+        bgenerator = iphocode.QuestionBarcodeGen(q.exam, q, s, qcode='C')
+        page = pdf.add_barcode(question_pdf, bgenerator)
+        doc_pages = pdf.get_num_pages(page)
+        meta['num_pages'] += doc_pages
+        meta['barcode_num_pages'] += doc_pages
+        all_barcodes.append(bgenerator.base)
+        all_docs.append(page)
+
+    for question in questions:
+        for sl in student_languages:
+            if question.is_answer_sheet() and not sl.with_answer:
+                continue
+
+            print 'Prepare', question, 'in', sl.language
+            trans = qquery.latest_version(question.pk, sl.language.pk) ## TODO: simplify latest_version, because question and language are already in memory
+            if not trans.lang.is_pdf:
+                trans_content, ext_resources = trans.qml.make_tex()
+                for r in ext_resources:
+                    if isinstance(r, tex.FigureExport):
+                        r.lang = sl.language
+                ext_resources.append(tex.TemplateExport('ipho_exam/tex_resources/ipho2016.cls'))
+                context = {
+                            'polyglossia' : sl.language.polyglossia,
+                            'polyglossia_options' : sl.language.polyglossia_options,
+                            'font'        : fonts.ipho[sl.language.font],
+                            'extraheader' : sl.language.extraheader,
+                            'lang_name'   : u'{} ({})'.format(sl.language.name, sl.language.delegation.country),
+                            'exam_name'   : u'{}'.format(question.exam.name),
+                            'code'        : u'{}{}'.format(question.code, question.position),
+                            'title'       : u'{} - {}'.format(question.exam.name, question.name),
+                            'is_answer'   : question.is_answer_sheet(),
+                            'document'    : trans_content,
+                          }
+                body = render_to_string('ipho_exam/tex/exam_question.tex', RequestContext(HttpRequest(), context)).encode("utf-8")
+                print 'Compile', question, student, sl.language
+                question_pdf = pdf.compile_tex(body, ext_resources)
+            else:
+                question_pdf = trans.node.pdf.read()
+
+            doc_pages = pdf.get_num_pages(question_pdf)
+            meta['num_pages'] += doc_pages
+            if question.is_answer_sheet():
+                bgenerator = iphocode.QuestionBarcodeGen(question.exam, question, sl.student)
+                page = pdf.add_barcode(question_pdf, bgenerator)
+                meta['barcode_num_pages'] += doc_pages
+                all_barcodes.append(bgenerator.base)
+                all_docs.append( page )
+            else:
+                all_docs.append(question_pdf)
+
+            if question.is_answer_sheet() and question.working_pages > 0:
+                context = {
+                            'polyglossia' : 'english',
+                            'polyglossia_options' : '',
+                            'font'        : fonts.ipho['notosans'],
+                            'extraheader' : '',
+                            # 'lang_name'   : u'{} ({})'.format(sl.language.name, sl.language.delegation.country),
+                            'exam_name'   : u'{}'.format(question.exam.name),
+                            'code'        : u'{}{}'.format('W', question.position),
+                            'title'       : u'{} - {}'.format(question.exam.name, question.name),
+                            'is_answer'   : question.is_answer_sheet(),
+                            'pages'       : range(question.working_pages),
+                          }
+                body = render_to_string('ipho_exam/tex/exam_blank.tex', RequestContext(HttpRequest(), context)).encode("utf-8")
+                question_pdf = pdf.compile_tex(body, [
+                    tex.TemplateExport('ipho_exam/tex_resources/ipho2016.cls')
+                ])
+                bgenerator = iphocode.QuestionBarcodeGen(question.exam, question, sl.student, qcode='W')
+                page = pdf.add_barcode(question_pdf, bgenerator)
+
+                doc_pages = pdf.get_num_pages(page)
+                meta['num_pages'] += doc_pages
+                meta['barcode_num_pages'] += doc_pages
+                all_barcodes.append(bgenerator.base)
+                all_docs.append(page)
+
+        exam_id = question.exam.pk
+        position = question.position
+
+    if all_same(all_barcodes):
+        meta['barcode_base'] = all_barcodes[0] or None
+    else:
+        meta['barcode_base'] = ','.join(all_barcodes)
+
+    filename = u'{}_EXAM-{}-{}.pdf'.format(sl.student.code, exam_id, position)
+    final_doc = pdf.concatenate_documents(all_docs)
+    meta['filename'] = filename
+    meta['etag'] = md5(final_doc).hexdigest()
+    if commit:
+        try:
+            doc_task = models.DocumentTask.objects.get(task_id=self.request.id)
+            doc = doc_task.document
+            contentfile = ContentFile(doc_pdf)
+            contentfile.name = meta['filename']
+            doc.file = contentfile
+            doc.num_pages = meta['num_pages']
+            doc.barcode_num_pages = meta['barcode_num_pages']
+            doc.barcode_base = meta['barcode_base']
+            doc.save()
+            doc_task.delete()
+        except models.DocumentTask.DoesNotExist:
+            pass
+    return final_doc, meta
