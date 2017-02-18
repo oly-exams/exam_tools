6ccaf2135fe5aa12f09a29bb76517223f49c414b
Matpi <wenger.quentin@bluewin.ch>
2017-02-19 00:32:51 +0100

    Switch to bower for ckeditor plugins.

--- bower_postinstall.sh
+++ bower_postinstall.sh
@@ -18,6 +18,20 @@ cp -r $STATIC_BOWER/bootstrap-switch/dist/ $STATIC/bootstrap-switch/
 # ckeditor
 cp -r $STATIC_BOWER/ckeditor/ $STATIC/ckeditor/
 
+# ckeditor_plugins
+cp -r $STATIC_BOWER/mathedit/mathedit/ $STATIC/ckeditor_plugins/mathedit/
+cp -r $STATIC_BOWER/mathjax_4.5.11/ $STATIC/ckeditor_plugins/mathjax-mathquill/
+cp -r $STATIC_BOWER/VirtualKeyboard.ckeditor.3.7.2.tar/ $STATIC/ckeditor_plugins/Jsvk
+# note: bg.js, de-ch.js, eu.js, id.js, ko.js, ug.js were initially not in the repo
+patch $STATIC/ckeditor_plugins/mathedit/plugin.js $STATIC/ckeditor_plugins/me_plugin.diff
+patch $STATIC/ckeditor_plugins/mathjax-mathquill/plugin.js $STATIC/ckeditor_plugins/mj_plugin.diff
+patch $STATIC/ckeditor_plugins/mathjax-mathquill/dev/mathjax.html $STATIC/ckeditor_plugins/mj_dev_mathjax.diff
+patch $STATIC/ckeditor_plugins/mathjax-mathquill/dialogs/mathjax.js $STATIC/ckeditor_plugins/mj_dialogs_mathjax.diff
+patch $STATIC/ckeditor_plugins/mathjax-mathquill/samples/mathjax.html $STATIC/ckeditor_plugins/mj_samples_mathjax.diff
+mv $STATIC/ckeditor_plugins/mathjax-mathquill/dialogs/mathjax.js $STATIC/ckeditor_plugins/mathjax-mathquill/dialogs/mathjax-mathquill.js
+mv $STATIC/ckeditor_plugins/mathjax-mathquill/icons/mathjax.png $STATIC/ckeditor_plugins/mathjax-mathquill/icons/mathjax-mathquill.png
+mv $STATIC/ckeditor_plugins/mathjax-mathquill/icons/hidpi/mathjax.png $STATIC/ckeditor_plugins/mathjax-mathquill/icons/hidpi/mathjax-mathquill.png
+
 # moment.js
 cp $STATIC_BOWER/moment/min/moment.min.js $STATIC/
 
