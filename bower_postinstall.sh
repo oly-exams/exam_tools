3ddcaee4aa53f3acdd66c7dbeb00f3b6259e62b6
Matpi <wenger.quentin@bluewin.ch>
2017-02-18 21:28:42 +0100

    Change bower dependencies for bootstrap and bootstrap-datetimepicker.

--- bower_postinstall.sh
+++ bower_postinstall.sh
@@ -7,6 +7,10 @@ STATIC=static
 # ace
 cp -r $STATIC_BOWER/ace-builds/src-min/ $STATIC/ace/
 
+# bootstrap and bootstrap-datetimepicker
+cp -r $STATIC_BOWER/bootstrap/dist/ $STATIC/bootstrap/
+cp $STATIC_BOWER/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css $STATIC/bootstrap/css/
+cp $STATIC_BOWER/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js $STATIC/bootstrap/js/
 
 # moment.js
 cp $STATIC_BOWER/moment/min/moment.min.js $STATIC/
