b421f0497f3862af5a70727fe10aca043080c607
Quentin Wenger <wenger.quentin@bluewin.ch>
2017-02-19 10:29:40 +0100

    Simplify bower_postinstall

--- bower_postinstall.sh
+++ bower_postinstall.sh
@@ -5,8 +5,7 @@ STATIC=static
 
 
 # ace
-cp -r $STATIC_BOWER/ace-builds/src-min $STATIC/
-mv $STATIC/src-min $STATIC/ace
+cp -r $STATIC_BOWER/ace-builds/src-min $STATIC/ace
 
 # bootstrap and bootstrap-datetimepicker
 cp -r $STATIC_BOWER/bootstrap/dist $STATIC/
@@ -15,8 +14,7 @@ cp $STATIC_BOWER/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimep
 cp $STATIC_BOWER/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js $STATIC/bootstrap/js/
 
 # bootstrap-switch
-cp -r $STATIC_BOWER/bootstrap-switch/dist $STATIC/
-mv $STATIC/dist $STATIC/bootstrap-switch
+cp -r $STATIC_BOWER/bootstrap-switch/dist $STATIC/bootstrap-switch
 
 # ckeditor
 cp -r $STATIC_BOWER/ckeditor $STATIC/
@@ -24,10 +22,8 @@ patch $STATIC/ckeditor/ckeditor.js $STATIC/ckeditor/ck_editor.diff
 
 # ckeditor_plugins
 cp -r $STATIC_BOWER/mathedit/mathedit $STATIC/ckeditor_plugins/
-cp -r $STATIC_BOWER/mathjax_4.5.11 $STATIC/ckeditor_plugins/
-mv $STATIC/ckeditor_plugins/mathjax_4.5.11 $STATIC/ckeditor_plugins/mathjax-mathquill
-cp -r $STATIC_BOWER/VirtualKeyboard.ckeditor.3.7.2.tar $STATIC/ckeditor_plugins/
-mv $STATIC/ckeditor_plugins/VirtualKeyboard.ckeditor.3.7.2.tar $STATIC/ckeditor_plugins/Jsvk
+cp -r $STATIC_BOWER/mathjax_4.5.11 $STATIC/ckeditor_plugins/mathjax-mathquill
+cp -r $STATIC_BOWER/VirtualKeyboard.ckeditor.3.7.2.tar $STATIC/ckeditor_plugins/Jsvk
 # note: bg.js, de-ch.js, eu.js, id.js, ko.js, ug.js were initially not in the repo
 patch $STATIC/ckeditor_plugins/mathedit/plugin.js $STATIC/ckeditor_plugins/me_plugin.diff
 patch $STATIC/ckeditor_plugins/mathjax-mathquill/plugin.js $STATIC/ckeditor_plugins/mj_plugin.diff
