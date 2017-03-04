35ceb8703efd53268615029597c4168444b1d1df
Michele Dolfi <michele.dolfi@gmail.com>
2017-03-04 20:31:48 +0100

    prefer get_or_create

--- scripts/prod_04_import_student_seating.py
+++ scripts/prod_04_import_student_seating.py
@@ -34,8 +34,8 @@ def main(input):
         try:
             student = Student.objects.get(code=row['individual_id'])
 
-            Place(student=student, exam=theory, name=row['seat_theory']).save()
-            Place(student=student, exam=experiment, name=row['seat_experiment']).save()
+            Place.objects.get_or_create(student=student, exam=theory, name=row['seat_theory'])
+            Place.objects.get_or_create(student=student, exam=experiment, name=row['seat_experiment'])
 
             print row['individual_id'], '.....', 'imported.'
         except Student.DoesNotExist:
