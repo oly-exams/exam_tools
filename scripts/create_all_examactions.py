c760b2a4df5738c30af0ac0a1be9bbefa9c8ab96
Michele Dolfi <michele.dolfi@gmail.com>
2017-03-04 19:16:03 +0100

    improve maintenance scripts

---
+++ scripts/create_all_examactions.py
@@ -0,0 +1,31 @@
+# Exam Tools
+#
+# Copyright (C) 2014 - 2017 Oly Exams Team
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'exam_tools.settings'
+
+import django
+django.setup()
+
+from ipho_exam.models import *
+
+exams = Exam.objects.all()
+for exam in Exam.objects.all():
+    for delegation in Delegation.objects.all():
+        for action,_ in ExamAction.ACTION_CHOICES:
+            exam_action, _ = ExamAction.objects.get_or_create(exam=exam, delegation=delegation, action=action)
+
