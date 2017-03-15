ee1decc7563a3e29b09cdda7c22761b98a89b588
Michele Dolfi <michele.dolfi@gmail.com>
2017-03-15 03:45:39 +0100

    Closes #305. Use SlugField for related models

--- ipho_exam/serializers.py
+++ ipho_exam/serializers.py
@@ -20,6 +20,8 @@ from ipho_exam.models import Document
 
 
 class DocumentSerializer(serializers.ModelSerializer):
+    student = serializers.SlugRelatedField(slug_field='code', read_only=True)
+    exam    = serializers.SlugRelatedField(slug_field='name', read_only=True)
     class Meta:
         model = Document
         fields = '__all__'
