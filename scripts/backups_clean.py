dfe7aaf745882facbe14c8751155bc8d97548b3f
Michele Dolfi <michele.dolfi@gmail.com>
2017-04-24 21:24:12 +0200

    fix backup script for new dbbackup version

--- scripts/backups_clean.py
+++ scripts/backups_clean.py
@@ -27,21 +27,21 @@ import datetime
 
 from dbbackup import settings as dbbackup_settings
 from dbbackup import utils
-from dbbackup.storage.base import BaseStorage, StorageError
+from dbbackup.storage import Storage, StorageError
 
 import logging
 logger = logging.getLogger('exam_tools.backups')
 
 def clean_old_backups():
-    storage = BaseStorage.storage_factory()
-    keep_days = 4
+    storage = Storage()
+    keep_delta = datetime.timedelta(days=4)
     files = storage.list_backups()
     now = datetime.datetime.now()
     files = sorted(files, key=utils.filename_to_date, reverse=True)
-    files_to_delete = [fi for fi in files if (now - utils.filename_to_date(fi)).days > keep_days]
-    logger.info('Deleting {} files on {}. keep_days={}'.format( len(files_to_delete), len(files), keep_days))
+    files_to_delete = [fi for fi in files if (now - utils.filename_to_date(fi)) > keep_delta]
+    logger.info('Deleting {} files on {}. keep_delta={}'.format( len(files_to_delete), len(files), keep_delta))
     for filename in files_to_delete:
-        logging.info('Removing ' +filename)
+        logger.info('Removing ' +filename)
         storage.delete_file(filename)
 
 if __name__ == '__main__':
