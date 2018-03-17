8959baa22637229c031ad346789f498f84be3ed9
Dominik Gresch <greschd@gmx.ch>
2018-03-17 17:50:22 +0100

    Add unicode_literals to some models.

--- ipho_exam/models.py
+++ ipho_exam/models.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 
 from builtins import object
 from builtins import str
