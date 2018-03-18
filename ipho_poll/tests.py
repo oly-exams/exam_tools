3b20abbd9d43c1f182b2f746ef70d0b477315e62
Thomas Uehlinger <thomas.uehlinger@gmail.com>
2018-03-18 09:12:50 +0100

    Update copyright dite

--- ipho_poll/tests.py
+++ ipho_poll/tests.py
@@ -1,7 +1,7 @@
 from __future__ import division
 # Exam Tools
 #
-# Copyright (C) 2014 - 2017 Oly Exams Team
+# Copyright (C) 2014 - 2018 Oly Exams Team
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
