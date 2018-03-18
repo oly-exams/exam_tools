9197558409f24fe5f675e5404f8cf3a279b02b3c
Dominik Gresch <greschd@gmx.ch>
2018-03-18 15:37:54 +0100

    Add migration for AttributeChange

--- ipho_exam/migrations/0090_figure_natural_key.py
+++ ipho_exam/migrations/0090_figure_natural_key.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
+from collections import Mapping
+
 from django.db import migrations, models
 import ipho_exam.utils.natural_id
 from ipho_exam import qml
 from ipho_exam.qml import QMLfigure, make_qml
 
-def migrate_figid(apps, qml_obj):
+def get_updated_figid(apps, figid):
     Figure = apps.get_model('ipho_exam', 'Figure')
+    fig = Figure.objects.get(pk=int(figid))
+    return fig.natural_key
+
+def migrate_version_node(apps, qml_obj):
     if isinstance(qml_obj, QMLfigure):
-        fig = Figure.objects.get(pk=int(qml_obj.attributes['figid']))
-        qml_obj.attributes['figid'] = fig.natural_key
+        qml_obj.attributes['figid'] = get_updated_figid(apps, qml_obj.attributes['figid'])
     for child in qml_obj.children:
-        migrate_figid(apps, child)
+        migrate_version_node(apps, child)
+
+def migrate_attr_change(apps, mapping):
+    for key, value in mapping.items():
+        if key == 'figid':
+            mapping[key] = get_updated_figid(apps, value)
+        elif isinstance(value, Mapping):
+            migrate_attr_change(apps, mapping=value)
 
 def forwards_func(apps, schema_editor):
     Figure = apps.get_model('ipho_exam', 'Figure')
@@ -23,13 +36,19 @@ def forwards_func(apps, schema_editor):
     VersionNode = apps.get_model('ipho_exam', 'VersionNode')
     for version_node in VersionNode.objects.all():
         version_qml = make_qml(version_node)
-        migrate_figid(apps, version_qml)
+        migrate_version_node(apps, version_qml)
         version_node.text = qml.xml2string(version_qml.make_xml())
         version_node.save()
 
+    AttributeChange = apps.get_model('ipho_exam', 'AttributeChange')
+    for attr_change in AttributeChange.objects.all():
+        attr_change_mapping = json.loads(attr_change.content)
+        migrate_attr_change(apps, attr_change_mapping)
+        attr_change.content = json.dumps(attr_change_mapping)
+        attr_change.save()
+
 def backwards_func(apps, schema_editor):
     pass
-
 
 class Migration(migrations.Migration):
 
