8f32913f00c56fc53a16a046377297dac3972b3f
Quentin Wenger <wenger.quentin@bluewin.ch>
2018-05-04 12:31:29 +0200

    Make official marking input fields wider.

--- ipho_marking/views.py
+++ ipho_marking/views.py
@@ -649,6 +649,7 @@ def official_marking_detail(request, question_id, delegation_id):
         )
         for j, f in enumerate(form):
             f.fields['points'].widget.attrs['tabindex'] = i*len(metas) + j + 1
+            f.fields['points'].widget.attrs['style'] = 'width:100px'
 
         # TODO: accept only submission without None
         all_valid = all_valid and form.is_valid()
