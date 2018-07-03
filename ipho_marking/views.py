58c22b485e7d7ab87266bc22472d30a8f85b8525
Dominik Gresch <greschd@gmx.ch>
2018-07-03 20:09:25 +0200

    Add option to get totals from marking export.

--- ipho_marking/views.py
+++ ipho_marking/views.py
@@ -26,7 +26,7 @@ from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 from django.core.context_processors import csrf
 from crispy_forms.utils import render_crispy_form
 from django.template.loader import render_to_string
-from django.db.models import Sum
+from django.db.models import Sum, F
 
 from django.forms import modelformset_factory, inlineformset_factory
 
@@ -167,7 +167,12 @@ def staff_stud_detail(request, version, stud_id, question_id):
 
 
 @permission_required('ipho_core.is_marker')
-def export(request):
+def export_with_total(request):
+    return export(request, include_totals=True)
+
+
+@permission_required('ipho_core.is_marker')
+def export(request, include_totals=False):
     versions = request.GET.get('v', 'O,D,F').split(',')
     exam_id = request.GET.get('exam', False)
 
@@ -180,6 +185,15 @@ def export(request):
     mmeta = MarkingMeta.objects.all().order_by('question__exam', 'question__position', 'position')
     for m in mmeta:
         title_row.append('{} - {} ({})'.format(m.question.name, m.name, m.max_points))
+    exams = Exam.objects.all()
+    questions = Question.objects.order_by('exam', 'position')
+    if include_totals:
+        for question in questions:
+            title_row.append('Question Total: {} - {}'.format(question.exam.name, question.name))
+        for exam in exams:
+            title_row.append('Exam Total: {}'.format(exam.name))
+        title_row.append('Student Total')
+
     writer.writerow(title_row)
 
     for student in Student.objects.all():
@@ -187,15 +201,30 @@ def export(request):
             row = [student.code, student.delegation.name, version]
             markings = Marking.objects.filter(
                 student=student, version=version
-            ).order_by('marking_meta__question__exam', 'marking_meta__question__position',
-                       'marking_meta__position').values_list(
-                           'points', flat=True
-                       )
-            row += markings
+            ).order_by('marking_meta__question__exam', 'marking_meta__question__position', 'marking_meta__position')
+            points = markings.values_list('points', flat=True)
+            for marking, meta in zip(markings, mmeta):
+                print(marking.marking_meta, meta)
+                assert marking.marking_meta == meta
+            row += points
+            if include_totals:
+                for question in questions:
+                    row.append(
+                        _get_total(markings.filter(marking_meta__question=question).values_list('points', flat=True))
+                    )
+                for exam in exams:
+                    row.append(_get_total(markings.filter(marking_meta__question__exam=exam)))
+                # try:
+                student_total = _get_total(markings)
+                row.append(student_total)
             row = ['-' if v is None else v for v in row]
             writer.writerow(row)
 
     return response
+
+
+def _get_total(filtered_markings):
+    return filtered_markings.aggregate(total=Sum(F('points')))['total']
 
 
 @permission_required('ipho_core.is_delegation')
@@ -346,16 +375,24 @@ def delegation_stud_edit(request, stud_id, question_id):
         form.save()
         students = delegation.student_set.all()
         stud_id_list = [str(s.id) for s in students]
-        next_stud_index = stud_id_list.index(stud_id)+1
+        next_stud_index = stud_id_list.index(stud_id) + 1
         next_stud_button = ''
         if next_stud_index < len(stud_id_list):
             next_stud_id = stud_id_list[next_stud_index]
-            next_stud_button = ' <a href="{}" class="btn btn-default btn-xs">next student</a> '.format(reverse('marking:delegation-stud-detail-edit',  kwargs={'stud_id':next_stud_id, 'question_id':question_id}))
+            next_stud_button = ' <a href="{}" class="btn btn-default btn-xs">next student</a> '.format(
+                reverse(
+                    'marking:delegation-stud-detail-edit', kwargs={
+                        'stud_id': next_stud_id,
+                        'question_id': question_id
+                    }
+                )
+            )
 
         ctx['msg'].append((
-            'alert-success',
-            '<strong>Success.</strong> Points have been saved.'+ next_stud_button + ' <a href="{}#details" class="btn btn-default btn-xs">back to summary</a> '.
-            format(reverse('marking:delegation-summary'))
+            'alert-success', '<strong>Success.</strong> Points have been saved.' + next_stud_button +
+            ' <a href="{}#details" class="btn btn-default btn-xs">back to summary</a> '.format(
+                reverse('marking:delegation-summary')
+            )
         ))
 
     documents = Document.objects.filter(student=student, exam=question.exam, position=question.position)
@@ -524,8 +561,9 @@ def delegation_confirm(request, exam_id):
 
     if any(m.points is None for m in markings_query):
         return HttpResponseForbidden(
-            'Some marks for {} are missing, please submit marks for all questions, subquestions and students before confirming!'.format(
-                exam.name))
+            'Some marks for {} are missing, please submit marks for all questions, subquestions and students before confirming!'.
+            format(exam.name)
+        )
 
     if request.POST:
         if 'agree-submit' in request.POST:
@@ -548,11 +586,17 @@ def delegation_confirm(request, exam_id):
         for k, g in itertools.groupby(markings_query, key=lambda m: m.marking_meta.question.pk)
     }
 
-    totals = {p: sum(totals_questions[k][p] for k in totals_questions)
-            for p in list(totals_questions.values())[0]}
+    totals = {p: sum(totals_questions[k][p] for k in totals_questions) for p in list(totals_questions.values())[0]}
 
-    ctx = {'exam': exam, 'questions': questions, 'markings': markings, 'metas': metas,
-        'totals_questions':totals_questions, 'totals':totals, 'form_error': form_error}
+    ctx = {
+        'exam': exam,
+        'questions': questions,
+        'markings': markings,
+        'metas': metas,
+        'totals_questions': totals_questions,
+        'totals': totals,
+        'form_error': form_error
+    }
     return render(request, 'ipho_marking/delegation_confirm.html', ctx)
 
 
@@ -596,7 +640,7 @@ def moderation_detail(request, question_id, delegation_id):
             queryset=Marking.objects.filter(marking_meta=metas, student=student, version='F')
         )
         for j, f in enumerate(form):
-            f.fields['points'].widget.attrs['tabindex'] = i*len(metas) + j + 1
+            f.fields['points'].widget.attrs['tabindex'] = i * len(metas) + j + 1
             f.fields['points'].widget.attrs['style'] = 'width:60px'
 
         # TODO: accept only submission without None
@@ -606,7 +650,8 @@ def moderation_detail(request, question_id, delegation_id):
         student_forms.append((
             student, form, markings_official,
             sum((m.points for m in markings_official if m.points is not None), decimal.Decimal(0)),
-            sum((m.points for m in markings_delegation if m.points is not None), decimal.Decimal(0))))
+            sum((m.points for m in markings_delegation if m.points is not None), decimal.Decimal(0))
+        ))
         marking_forms.append(zip(markings_official, markings_delegation, form))
 
     if all_valid:
@@ -621,15 +666,20 @@ def moderation_detail(request, question_id, delegation_id):
             )
         )
     # TODO: display errors
-    ctx = {'question': question, 'delegation': delegation, 'student_forms': student_forms, 'marking_forms': list(zip(metas, zip(*marking_forms))), 'request': request, 'max_points_sum': sum(m.max_points for m in metas)}
+    ctx = {
+        'question': question,
+        'delegation': delegation,
+        'student_forms': student_forms,
+        'marking_forms': list(zip(metas, zip(*marking_forms))),
+        'request': request,
+        'max_points_sum': sum(m.max_points for m in metas)
+    }
     return render(request, 'ipho_marking/moderation_detail.html', ctx)
 
 
 @permission_required('ipho_core.is_marker')
 def official_marking_index(request, question_id=None):
-    questions = Question.objects.filter(
-        exam__hidden=False, type=Question.ANSWER
-    ).order_by('exam__code', 'position')
+    questions = Question.objects.filter(exam__hidden=False, type=Question.ANSWER).order_by('exam__code', 'position')
     question = None if question_id is None else get_object_or_404(Question, id=question_id)
     delegations = Delegation.objects.all()
     ctx = {'questions': questions, 'question': question, 'delegations': delegations}
@@ -657,7 +707,7 @@ def official_marking_detail(request, question_id, delegation_id):
             queryset=Marking.objects.filter(marking_meta=metas, student=student, version='O')
         )
         for j, f in enumerate(form):
-            f.fields['points'].widget.attrs['tabindex'] = i*len(metas) + j + 1
+            f.fields['points'].widget.attrs['tabindex'] = i * len(metas) + j + 1
             f.fields['points'].widget.attrs['style'] = 'width:100px'
 
         # TODO: accept only submission without None
@@ -671,14 +721,22 @@ def official_marking_detail(request, question_id, delegation_id):
             form.save()
         return HttpResponseRedirect(
             reverse(
-                'marking:official-marking-confirmed', kwargs={
+                'marking:official-marking-confirmed',
+                kwargs={
                     'question_id': question.pk,
                     'delegation_id': delegation.pk
                 }
             )
         )
     # TODO: display errors
-    ctx = {'question': question, 'delegation': delegation, 'student_forms': student_forms, 'marking_forms': list(zip(metas, zip(*(f[1] for f in student_forms)))), 'request': request, 'max_points_sum': sum(m.max_points for m in metas)}
+    ctx = {
+        'question': question,
+        'delegation': delegation,
+        'student_forms': student_forms,
+        'marking_forms': list(zip(metas, zip(*(f[1] for f in student_forms)))),
+        'request': request,
+        'max_points_sum': sum(m.max_points for m in metas)
+    }
     return render(request, 'ipho_marking/official_marking_detail.html', ctx)
 
 
