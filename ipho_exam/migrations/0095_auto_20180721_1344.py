b0c478630b76061c92089458aa70db5f48d8233f
sebastian.stengele <sebastian.stengele@gmail.com>
2018-07-21 12:47:36 +0100

    add rejected option to feedbacks

---
+++ ipho_exam/migrations/0095_auto_20180721_1344.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ipho_exam', '0094_document_timestamp'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='feedback',
+            name='status',
+            field=models.CharField(choices=[('S', 'Submitted'), ('V', 'Scheduled for voting'), ('I', 'Implemented'), ('T', 'Settled'), ('R', 'Rejected')], default='S', max_length=1),
+        ),
+    ]
