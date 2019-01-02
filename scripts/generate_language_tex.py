1e6c5e6acc20520721017f60f54e000c7d103e40
Thomas Uehlinger <thomas.uehlinger@gmail.com>
2019-01-02 20:56:23 +0100

    Switch to IPhO2019 preliminary logo

--- scripts/generate_language_tex.py
+++ scripts/generate_language_tex.py
@@ -148,6 +148,6 @@ def export_all(logo_file, names=('Theory', 'Experiment')):
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        export_all(logo_file='ipho18_logo.png', names=sys.argv[1:])
+        export_all(logo_file='ipho19_logo.png', names=sys.argv[1:])
     else:
-        export_all(logo_file='ipho18_logo.png')
+        export_all(logo_file='ipho19_logo.png')
