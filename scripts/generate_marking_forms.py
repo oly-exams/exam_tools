d47a613f83af81984ab2f89819167370ea9f89a5
Anian Altherr <anian95.altherr@hotmail.com>
2019-07-13 13:13:57 +0300

    added script to generate forms for moderation

---
+++ scripts/generate_marking_forms.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import pdfkit
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'exam_tools.settings'
+import sys
+
+import django
+django.setup()
+from django.conf import settings
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpRequest
+
+from django.core.urlresolvers import reverse
+from django.template.loader import render_to_string
+
+from ipho_core.models import Delegation, Student
+from ipho_exam.models import Exam, Question 
+
+from django.db.models import Sum, F
+from ipho_marking.models import MarkingMeta, Marking
+from ipho_marking.forms import ImportForm, PointsForm
+from django.forms import modelformset_factory, inlineformset_factory
+import decimal
+
+def moderation_detail(question_id, delegation_id, request=HttpRequest()):
+    question = get_object_or_404(Question, id=question_id, exam__hidden=False, exam__moderation_active=True)
+    delegation = get_object_or_404(Delegation, id=delegation_id)
+    metas = MarkingMeta.objects.filter(question=question)
+    students = delegation.student_set.all()
+
+    student_forms = []
+    marking_forms = []
+    all_valid = True
+    with_errors = False
+    for i, student in enumerate(students):
+        markings_official = Marking.objects.filter(
+            student=student, marking_meta__in=metas, version='O'
+        ).order_by('marking_meta__position')
+        markings_delegation = Marking.objects.filter(
+            student=student, marking_meta__in=metas, version='D'
+        ).order_by('marking_meta__position')
+
+        FormSet = modelformset_factory(
+            Marking, form=PointsForm, fields=['points'], extra=0, can_delete=False, can_order=False
+        )
+        form = FormSet(
+            request.POST or None,
+            prefix='Stud-{}'.format(student.pk),
+            queryset=Marking.objects.filter(marking_meta__in=metas, student=student, version='F')
+        )
+        for j, f in enumerate(form):
+            f.fields['points'].widget.attrs['tabindex'] = i * len(metas) + j + 1
+            f.fields['points'].widget.attrs['style'] = 'width:60px'
+
+        # TODO: accept only submission without None
+        all_valid = all_valid and form.is_valid()
+        with_errors = with_errors or form.errors
+
+        student_forms.append((
+            student, form, markings_official,
+            sum((m.points for m in markings_official if m.points is not None), decimal.Decimal(0)),
+            sum((m.points for m in markings_delegation if m.points is not None), decimal.Decimal(0))
+        ))
+        marking_forms.append(zip(markings_official, markings_delegation, form))
+
+    if all_valid:
+        for _, form, _, _, _ in student_forms:
+            form.save()
+        return HttpResponseRedirect(
+            reverse(
+                'marking:moderation-confirmed', kwargs={
+                    'question_id': question.pk,
+                    'delegation_id': delegation.pk
+                }
+            )
+        )
+    # TODO: display errors
+    ctx = {
+        'question': question,
+        'delegation': delegation,
+        'student_forms': student_forms,
+        'marking_forms': list(zip(metas, zip(*marking_forms))),
+        'request': request,
+        'max_points_sum': sum(m.max_points for m in metas)
+    }
+    body =  render_to_string('ipho_marking/moderation_detail.html', context=ctx, request=request)
+
+    start = body.find('<!-- S')
+    intermediate = body.find('sr-only')
+    end = body.find('<div class="container', intermediate)
+
+    start2 = body.find('[> /container')
+    end2 = start2 + 16
+
+    file = open("html/test-{}-{}.html".format(question_id, delegation_id),"w")
+    file.write(body[:start] + body[end:start2] + body[end2:])
+    file.close()
+    pdfkit.from_file("html/test-{}-{}.html".format(question_id, delegation_id), 'pdf/test-{}-{}.pdf'.format(question_id, delegation_id))
+
+
+answer_sheets = Question.objects.filter(exam__hidden=False, exam__moderation_active=True, code='A')
+all_delegations = Delegation.objects.all()
+
+for question in answer_sheets:
+    for delegation in all_delegations:
+        print(question.pk, delegation.pk)
+        try:
+            moderation_detail(question.pk,delegation.pk)
+        except:
+            pass
+
