381959b92840ad49ed8cc4496b2b31dbb782a9db
Quentin Wenger <wenger.quentin@bluewin.ch>
2019-07-14 13:39:10 +0200

    Rewrite mkdocs overrides path handling to rely less on mkdocs internals.

--- scripts/mkdocs_custom.py
+++ scripts/mkdocs_custom.py
@@ -6,115 +6,46 @@ import shutil
 from functools import partial
 import argparse
 
-from mkdocs import config
+from mkdocs import config as mk_config
 from mkdocs.exceptions import ConfigurationError
 from mkdocs.plugins import BasePlugin
 from mkdocs.commands import build
 from mkdocs.commands import serve
-from mkdocs.structure import pages
-from mkdocs.structure.files import File
-from mkdocs.utils import urlparse
-from mkdocs.utils import urlunparse
-
-import markdown
-from markdown.util import AMP_SUBSTITUTE
-
-from mkdocs.structure.toc import get_toc
-
-
-
-class _RelativePathTreeprocessor(pages._RelativePathTreeprocessor):
-    def __init__(self, file, files, config):
-        self.file = file
-        self.files = files
-        self.config = config
-
-    def path_to_url(self, url):
-        scheme, netloc, path, params, query, fragment = urlparse(url)
-
-        if (scheme or netloc or not path or url.startswith('/')
-                or AMP_SUBSTITUTE in url or '.' not in os.path.split(path)[-1]):
-            # Ignore URLs unless they are a relative link to a source file.
-            # AMP_SUBSTITUTE is used internally by Markdown only for email.
-            # No '.' in the last part of a path indicates path does not point to a file.
-            return url
-
-        # Determine the filepath of the target.
-        target_path = os.path.join(os.path.dirname(self.file.src_path), path)
-        target_path = os.path.normpath(target_path).lstrip(os.sep)
-
-        # Validate that the target exists in files collection.
-        if target_path not in self.files:
-            # XXX: modified from mkdocs source:
-            target_path = os.path.normpath(os.path.relpath(
-                os.path.join(self.config['docs_dir'], target_path),
-                self.config['event_folder_path'],
-            ))
-            if target_path not in self.files:
-                return url
-        target_file = self.files.get_file_from_path(target_path)
-        path = target_file.url_relative_to(self.file)
-        components = (scheme, netloc, path, params, query, fragment)
-        return urlunparse(components)
-
-
-class _RelativePathExtension(pages._RelativePathExtension):
-    def __init__(self, file, files, config):
-        self.file = file
-        self.files = files
-        self.config = config
-
-    def extendMarkdown(self, md, md_globals):
-        relpath = _RelativePathTreeprocessor(self.file, self.files, self.config)
-        md.treeprocessors.add("relpath", relpath, "_end")
-
-
-
-def render(self, config, files):
-    """
-    Convert the Markdown source file to HTML as per the config.
-    """
-
-    extensions = [
-        _RelativePathExtension(self.file, files, config)
-    ] + config['markdown_extensions']
-
-    md = markdown.Markdown(
-        extensions=extensions,
-        extension_configs=config['mdx_configs'] or {}
-    )
-    self.content = md.convert(self.markdown)
-    self.toc = get_toc(getattr(md, 'toc', ''))
+from mkdocs.structure import files
 
 
 
 class OverrideFilesPlugin(BasePlugin):
-    def on_files(self, files, config):
+    def on_files(self, nav_files, config):
         if os.path.isdir(config['event_folder_path']):
-            orig_rel_paths = [nav_file.src_path for nav_file in files]
-            for nav_file in files:
-                nav_file_name = os.path.basename(nav_file.src_path)
-                override_path = os.path.join(config['event_folder_path'], nav_file.src_path)
-                if os.path.isfile(override_path):
-                    nav_file.abs_src_path = os.path.normpath(os.path.abspath(override_path))
-                    nav_file.src_path = os.path.normpath(os.path.relpath(nav_file.abs_src_path, config['docs_dir']))
-                    print('Override path: {}'.format(nav_file.src_path))
-            for folder, __, nav_over_files in os.walk(config['event_folder_path']):
-                for nav_over_file in nav_over_files:
-                    nav_over_filepath = os.path.normpath(os.path.relpath(os.path.join(folder, nav_over_file), config['event_folder_path']))
-                    if nav_over_filepath not in orig_rel_paths:
-                        abs_path = os.path.normpath(os.path.abspath(os.path.join(folder, nav_over_file)))
-                        f = File(
-                            os.path.relpath(abs_path, config['event_folder_path']),
+            fs = []
+            orig_rel_paths = [nav_file.src_path for nav_file in nav_files]
+            for nav_file in nav_files:
+                abs_path = os.path.join(config['event_folder_path'], nav_file.src_path)
+                if os.path.isfile(abs_path):
+                    f = files.File(
+                        nav_file.src_path,
+                        os.path.abspath(config['event_folder_path']),
+                        config['site_dir'],
+                        config['use_directory_urls'],
+                    )
+                    fs.append(f)
+                    print('Override path: {}'.format(f.src_path))
+                else:
+                    fs.append(nav_file)
+            for folder, __, nav_new_files in os.walk(config['event_folder_path']):
+                for nav_new_file in nav_new_files:
+                    nav_new_filepath = os.path.normpath(os.path.relpath(os.path.join(folder, nav_new_file), config['event_folder_path']))
+                    if nav_new_filepath not in orig_rel_paths:
+                        f = files.File(
+                            nav_new_filepath,
                             os.path.abspath(config['event_folder_path']),
                             config['site_dir'],
                             config['use_directory_urls'],
                         )
-                        files.append(f)
+                        fs.append(f)
                         print('New path: {}'.format(f.src_path))
-    def on_pre_page(self, page, config, *args, **kwargs):
-        # Yup, monkeypatching is hacky.
-        page.render = partial(render, page)
+            return files.Files(fs)
 
 
 
@@ -130,7 +61,7 @@ if __name__ == '__main__':
 
     def builder():
         config_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'mkdocs.yml')
-        cfg = config.Config(schema=config.DEFAULT_SCHEMA, config_file_path=config_file_path)
+        cfg = mk_config.Config(schema=mk_config.DEFAULT_SCHEMA, config_file_path=config_file_path)
         with open(config_file_path, 'rb') as config_file:
             cfg.load_file(config_file)
         if args.site_dir:
