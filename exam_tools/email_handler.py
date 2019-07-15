8f1c8cd9d60b39f7d1a6e52e7ac646fccccb5439
Quentin Wenger <wenger.quentin@bluewin.ch>
2019-07-15 17:37:58 +0200

    Update email handler to include information about users.

--- exam_tools/email_handler.py
+++ exam_tools/email_handler.py
@@ -16,44 +16,21 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.conf import settings
-from django.utils.encoding import force_text
-from django.views.debug import ExceptionReporter, get_exception_reporter_filter
 from django.utils.log import AdminEmailHandler
 
 class EnhancedAdminEmailHandler(AdminEmailHandler):
-    # NOTE: most of this directly copied from AdminEmailHandler.
     def emit(self, record):
-        try:
-            request = record.request
-            subject = '%s (%s IP): %s' % (
-                record.levelname,
-                ('internal' if request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS
-                 else 'EXTERNAL'),
-                record.getMessage()
-            )
-            filter = get_exception_reporter_filter(request)
-            request_repr = '\n{}'.format(force_text(filter.get_request_repr(request)))
-            if request.user.is_authenticated:
+        self.current_record = record
+        super().emit(record)
+    def send_mail(self, subject, message, *args, **kwargs):
+        # Let's be a bit defensive here, it'd be bad to have errors in this method...
+        # (for reasons, see title text of xkcd.com/1163)
+        if hasattr(self, "current_record") and hasattr(self.current_record, "request"):
+            request = self.current_record.request
+            if hasattr(request, "user") and hasattr(request.user, "is_authenticated") and request.user.is_authenticated:
                 additional_info = "\n  Username: " + request.user.username
             else:
                 additional_info = "\n  Unauthenticated user"
-        except Exception:
-            subject = '%s: %s' % (
-                record.levelname,
-                record.getMessage()
-            )
-            request = None
-            request_repr = "unavailable"
-            additional_info = "unavailable"
-        subject = self.format_subject(subject)
-
-        if record.exc_info:
-            exc_info = record.exc_info
         else:
-            exc_info = (None, record.getMessage(), None)
-
-        message = "%s\n\nRequest repr(): %s\n\nAdditional information: %s" % (
-            self.format(record), request_repr, additional_info)
-        reporter = ExceptionReporter(request, is_email=True, *exc_info)
-        html_message = reporter.get_traceback_html() if self.include_html else None
-        self.send_mail(subject, message, fail_silently=True, html_message=html_message)
+            additional_info = "\n  Additional info: unavailable"
+        super().send_mail(subject, message + additional_info, *args, **kwargs)
