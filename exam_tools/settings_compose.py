4bba540f9b9725b6aa10f7e1952c0d95545d9289
Michele Dolfi <michele.dolfi@gmail.com>
2020-07-04 21:39:41 +0200

    add settings for docker compose

---
+++ exam_tools/settings_compose.py
@@ -0,0 +1,52 @@
+# Exam Tools
+#
+# Copyright (C) 2014 - 2018 Oly Exams Team
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+# Django settings for exam_tools project.
+from .settings_common import *
+
+
+EVENT_TEMPLATE_PATH = os.path.join(TEMPLATE_PATH, 'events', 'apho2019')
+
+DEBUG = True
+#TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+    ('Michele Dolfi', 'michele.dolfi@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+BROKER_URL = 'amqp://user:pass@rabbit:5672/'
+
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '^t-a=sbo_05wq!*(x4mpv7kw&u_n=5js$lwadn_yx(bzx*fzjw'
