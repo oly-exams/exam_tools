a323634598ff8565b60f24c2d3820f54050a6f33
mskoenz <mskoenz@gmx.net>
2020-10-26 17:43:21 +0100

    changed ci and docker structure

--- exam_tools/settings_testing.py
+++ exam_tools/settings_testing.py
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
+# pylint: disable=unused-wildcard-import
+
 import sys
 
 # Django settings for exam_tools project.
@@ -37,6 +40,7 @@ TEST_OUTPUT_VERBOSE = 2
 
 ADMINS = ()
 
+ALLOWED_HOSTS = ["*"]
 MANAGERS = ADMINS
 
 DATABASES = {
