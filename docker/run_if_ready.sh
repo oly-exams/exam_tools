431f3cdffb1abf96489a4350ee62ee53010e7475
mskoenz <mskoenz@gmx.net>
2020-11-05 19:37:44 +0100

    small fix in CI template

--- docker/run_if_ready.sh
+++ docker/run_if_ready.sh
@@ -3,8 +3,10 @@ set -e
 
 COMMAND=$1
 WAIT_FOR=$2
+
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=$(dirname $SCRIPT)
+
 for HOST_PORT in $WAIT_FOR
 do
     $SCRIPTPATH/wait-for-it.sh $HOST_PORT --timeout=60
