2e7f1521b5c00909595ffde9886f3e7ab7f29cd3
mskoenz <mskoenz@gmx.net>
2020-11-06 00:28:07 +0100

    refactored gitlab CI files

--- docker/dev_compose.sh
+++ docker/dev_compose.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
-# 2020 Oct: this is a convenience bash script to prevent users from having
-# to specify all partial docker-compose.yml files with -f.
-# docker-compose file v3 does not yet support a convenient way to
-# fuse partial files together. (v2 had extends, but that is not in v3)
-# use this command in any place you would use docker-compose.
-# The name was choosen to be easily tab-completed, not colliding with
-# the docker folder
+# this is identical to compose.sh, but does only include the services
+# needed for development, no pre_commit or testing_cypress. Furthermore
+# adds overwrite dev.yml.
+
 SCRIPT=$(readlink -f $0)
 SCRIPTPATH=$(dirname $SCRIPT)
 
 docker-compose -p "exam_tools" \
                --project-directory="$SCRIPTPATH" \
-               --env-file="$SCRIPTPATH/env/dev.yml" \
                -f "$SCRIPTPATH/django_server/docker-compose.yml" \
                -f "$SCRIPTPATH/celery_worker/docker-compose.yml" \
                -f "$SCRIPTPATH/rabbitmq/docker-compose.yml" \
