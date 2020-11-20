89cce60881f55e40ec26d5ca55a8fdf152217447
Sebastian Stengele <sebastian.stengele@gmail.com>
2020-11-20 18:16:07 +0100

    Add a first set of tests for final submission

--- e2e/cypress/integration/pdf_compilation/README.md
+++ e2e/cypress/integration/pdf_compilation/README.md
@@ -1,2 +1,7 @@
 Note:
 This batch is used to test pdf compilation and file downloads. It runs using a postgres db and with different test data.
+
+The tests in this section will need firefox to run properly. Hence you need to run the tests using:
+```bash
+cypress run --spec "cypress/integration/pdf_compilation/pdf_compile.js"  -b firefox --headless
+```
--- e2e/cypress/integration/pdf_compilation/pdf_compile.js
+++ e2e/cypress/integration/pdf_compilation/pdf_compile.js
@@ -1,4 +1,34 @@
 
+// Set this to false to disable pdf_checking
+// (e.g. to recreate pdfs after changes to the inital data)
+const check_pdf = true
+//
+//
+
+function test_final_pdf(stud_id, doc_pos) {
+    // Wait for pdf to compile
+    cy.get('#preview-'+String(stud_id)+'-'+String(doc_pos)+' i.fa',  { timeout: 60000 }).should('not.have.class', 'fa-spinner').then(($i)=>{
+        // Check icon again
+        cy.wrap($i).should('have.class', 'fa-file-pdf-o')
+        // Get Href
+        cy.wrap($i).parent().should('have.attr', 'href')
+        .then((href) => {
+            // Send request
+            cy.request({
+                url: href,
+                encoding: 'binary',
+            })
+            .then((response) => {
+                // Write file to disk and compare
+                cy.writeFile('cypress/pdfs/preview-'+String(stud_id)+'-'+String(doc_pos)+'.pdf', response.body, 'binary')
+                if(check_pdf){
+                    cy.exec('comparepdf --compare=appearance cypress/pdfs/preview-'+String(stud_id)+'-'+String(doc_pos)+'.pdf cypress/fixtures/pdfs/preview-'+String(stud_id)+'-'+String(doc_pos)+'.pdf')
+                }
+            })
+        })
+    })
+}
+
 
 describe('General', function() {
 
@@ -7,7 +37,8 @@ describe('General', function() {
         cy.route("GET", /exam\/pdf-task\/[^\/]*/).as("getPDFPendingPage");
     })
 
-    // Note that the usual beforeEach hook does not reset the postgres database, so we are only using one test.
+    // Note that the usual beforeEach hook does not reset the postgres database.
+    // So this test will change the state of the following ones. However they shouldn't interfere.
     it("Test pdf Compilation", function() {
         cy.login("admin", 1234)
         cy.visit('/exam/pdf/question/1/lang/1/v2').then(()=>{
@@ -26,8 +57,32 @@ describe('General', function() {
             })
             .then((response) => {
               cy.writeFile('cypress/pdfs/general_instr_v1.pdf', response.body, 'binary')
-              cy.exec("comparepdf --compare=appearance cypress/pdfs/general_instr_v1.pdf cypress/fixtures/general_instr_v1.pdf")
+              if(check_pdf){
+                cy.exec("comparepdf --compare=appearance cypress/pdfs/general_instr_v1.pdf cypress/fixtures/pdfs/general_instr_v1.pdf")
+              }
             })
         })
+    })
+
+    it.only("Test Final submission", function() {
+        cy.login("AUS", 1234)
+
+        cy.visit("/exam/submission/1/assign")
+
+        //choose languages
+        cy.get("#stud-6-languages-val-1").check()
+        cy.get("#stud-6-languages-val-2").check()
+        cy.get("#stud-6-answer_language-val-2").check()
+
+        cy.get("#stud-7-languages-val-1").check()
+        cy.get("#stud-7-answer_language-val-1").check()
+
+        cy.get('button[type="submit"]').should('contain', "Next").click()
+
+        cy.url().should('contain', "/exam/submission/1/confirm")
+
+        // We can only test the final submission for now, as there are missing images in the test data.
+        test_final_pdf(6, 0)
+        test_final_pdf(7, 0)
     })
 })
