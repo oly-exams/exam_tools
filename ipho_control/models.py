d120e606ff1f39ea1f46ce3afec08652ada9efa0
Thomas Uehlinger <thomas.uehlinger@gmail.com>
2021-04-11 16:53:34 +0200

    Fix loaddata for ExamPhase

--- ipho_control/models.py
+++ ipho_control/models.py
@@ -28,6 +28,9 @@ import ipho_control.phase_checks as phase_checks
 
 class ExamPhaseManager(models.Manager):
     def get_by_natural_key(self, name, exam):
+        # when using loaddata, the exam parameter is a list containing one item: replace exam by this one item
+        if hasattr(exam, '__iter__') and len(exam) == 1:
+            exam = exam[0]
         return self.get(name=name, exam=Exam.objects.get_by_natural_key(exam))
 
 
