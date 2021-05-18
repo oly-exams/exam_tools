8386bef4ada3a7ca01f61c7f1d91cd3a61044602
Thomas Uehlinger <thomas.uehlinger@gmail.com>
2021-05-18 19:29:04 +0200

    add local directory to path

--- scripts/create_all_examactions.py
+++ scripts/create_all_examactions.py
@@ -16,8 +16,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import sys
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "exam_tools.settings"
+sys.path.append(".")
 
 import django
 
