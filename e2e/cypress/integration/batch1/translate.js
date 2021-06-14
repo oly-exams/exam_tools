e79cf63f271b267adad5f85bf8ca3994d6391e18
Sebastian Stengele <sebastian.stengele@gmail.com>
2021-06-14 23:02:50 +0200

    update polyglossia in tests, enable video artifacts

--- e2e/cypress/integration/batch1/translate.js
+++ e2e/cypress/integration/batch1/translate.js
@@ -59,7 +59,7 @@ describe('Translation', function() {
 
         cy.get('#language-modal #id_style').select('Persian')
         cy.get('#id_direction').should('have.value', 'ltr')
-        cy.get('#id_polyglossia').should('have.value', 'farsi')
+        cy.get('#id_polyglossia').should('have.value', 'persian')
         cy.get('#id_polyglossia_options').should('have.value', 'numerals=western')
         cy.get('#id_font').should('have.value', 'notokufiarabic')
 
