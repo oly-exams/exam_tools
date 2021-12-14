34decb723d0040b9f697c605c9ee4485b4d57e1a
Sebastian Stengele <sebastian.stengele@gmail.com>
2021-12-14 20:37:08 +0100

    move marking tests to different batch

--- exam_tools/settings_testing_batch_4.py
+++ exam_tools/settings_testing_batch_4.py
@@ -18,32 +18,7 @@
 
 # pylint: disable=unused-wildcard-import
 
-import sys
 
-# Django settings for exam_tools project.
-from .settings_only_sqlite import *
+from .settings_testing import *
 
-# Printing paths for sanity's sake
-print("Settings directory:", SETTINGS_DIR, file=sys.stderr)
-print("Project root:", PROJECT_PATH, file=sys.stderr)
-print("Templates:", TEMPLATE_PATH, file=sys.stderr)
-print("Staticfiles Dirs:", STATICFILES_DIRS, file=sys.stderr)
-
-DEBUG = True
-# TEMPLATE_DEBUG = DEBUG
-
-TEST_RUNNER = "xmlrunner.extra.djangotestrunner.XMLTestRunner"
-TEST_OUTPUT_DIR = "unittest_reports"
-TEST_OUTPUT_FILE_NAME = "results.xml"
-TEST_OUTPUT_VERBOSE = 2
-
-CONTROL_SHOW_PHASES_ON_HOME = True
-ACCEPT_MARKS_BEFORE_MODERATION = True
-SIGN_OFF_FINAL_MARKS = True
-ADD_DELEGATION_WATERMARK = False
-
-
-ADMINS = ()
-
-ALLOWED_HOSTS = []
-MANAGERS = ADMINS
+ALLOW_NEGATIVE_MARKS = True
