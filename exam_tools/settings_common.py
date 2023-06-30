194433863c213bd1af0fd892a789915a276c35fa
Freakness109 <freakness109@gmail.com>
2023-06-30 16:27:05 +0400

    Settings: Add missing `django_filters` app
    This fixes issues with rendering API endpoints
    
    
    (cherry picked from commit 44e2855dec7dbd316ff8fa43fb2c28ed461f207a)

--- exam_tools/settings_common.py
+++ exam_tools/settings_common.py
@@ -583,6 +583,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     "django.contrib.admindocs",
     "django.contrib.humanize",
+    "django_filters",
     "crispy_forms",
     "django_ace",
     "django_celery_results",
