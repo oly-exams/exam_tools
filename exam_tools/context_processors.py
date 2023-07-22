e4c4b93f37b25ae029bb377057271ea82ab8cfeb
Sebastian Stengele <sebastian.stengele@gmail.com>
2023-07-22 19:46:33 +0200

    change favicon color depending on hostname

--- exam_tools/context_processors.py
+++ exam_tools/context_processors.py
@@ -15,10 +15,22 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 from django.conf import settings
 
 
 def ipho_context(request):
+    favicons_path = "favicons"
+    default_favicon_path = os.path.join(favicons_path, "default")
+    favicon_path = default_favicon_path
+    if any("secret" in hostname for hostname in settings.ALLOWED_HOSTS):
+        favicon_path = os.path.join(favicons_path, "secret")
+    if any(
+        partial_name in hostname
+        for hostname in settings.ALLOWED_HOSTS
+        for partial_name in ["dev", "test"]
+    ):
+        favicon_path = os.path.join(favicons_path, "dev")
     return {
         "DEMO_MODE": settings.DEMO_MODE,
         "DEMO_SIGN_UP": settings.DEMO_SIGN_UP,
@@ -28,4 +40,5 @@ def ipho_context(request):
         "STATIC_PATH": settings.STATIC_PATH,
         "DEBUG": getattr(settings, "DEBUG", False),
         "OFFICIALLY_SUPPORTED": settings.OFFICIALLY_SUPPORTED,
+        "FAVICON_PATH": favicon_path,
     }
